12b2a28b4250fc1cc79116c4739ded90f2dd329c
Pengfei He <hepengfe@gmail.com>
2022-04-06 13:45:02 -0700

    pylint

--- asyml_utilities/special_tokens.py
+++ asyml_utilities/special_tokens.py
@@ -3,11 +3,11 @@ __all__ = [
 ]
 
 class SpecialTokens:
-     r"""Special tokens, including :attr:`PAD`, :attr:`BOS`, :attr:`EOS`,
-     :attr:`UNK`. These tokens will by default have token ids 0, 1, 2, 3,
-     respectively.
-     """
-     PAD = "<PAD>"
-     BOS = "<BOS>"
-     EOS = "<EOS>"
-     UNK = "<UNK>"
+    r"""Special tokens, including :attr:`PAD`, :attr:`BOS`, :attr:`EOS`,
+    :attr:`UNK`. These tokens will by default have token ids 0, 1, 2, 3,
+    respectively.
+    """
+    PAD = "<PAD>"
+    BOS = "<BOS>"
+    EOS = "<EOS>"
+    UNK = "<UNK>"
